83f349e16e36f83e48b40ae60b3f0b503786f228
NatalieDiekmann <nataliia.diekmann@gmail.com>
2024-11-22 18:45:24 -0600

    yml6

--- src/test/java/PracticeTest.java
+++ src/test/java/PracticeTest.java
@@ -1,15 +1,20 @@
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import org.openqa.selenium.By;
 
 public class PracticeTest extends BaseTest {
 
+    //    @Test
+//    public void testStoreName() throws InterruptedException {
+//        System.out.println("Page Title is "+ driver.getTitle());
+//        String websiteName = driver.findElement(By.xpath("//h1/a[@href='https://askomdch.com/']")).getText();
+//
+//        Assert.assertEquals(websiteName, "AskOmDch");
+//
+//    }
     @Test
-    public void testStoreName() throws InterruptedException {
-        System.out.println("Page Title is "+ driver.getTitle());
-        String websiteName = driver.findElement(By.xpath("//h1/a[@href='https://askomdch.com/']")).getText();
-
-        Assert.assertEquals(websiteName, "AskOmDch");
+    public void testStoreName() {
+        String actualTitle = driver.getTitle();
+        Assert.assertEquals(actualTitle, "AskOmDch – Become a Selenium automation expert!", "Title does not match!");
 
     }
 }
